61acab359a5dd0ed1a53770af591392a9600756a
Krishan Maduwantha beruwalage <krishanmaduwantha2003@gmail.com>
2026-03-14 08:22:34 +0530

    Fix CORS for Vercel production

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
